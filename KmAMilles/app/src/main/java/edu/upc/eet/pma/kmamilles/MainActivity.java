f9ff5eb2562c7a3da7020b213515fee9517395fd
Pau Fernández <pau.fernandez@upc.edu>
2016-09-19 10:01:54 +0200

    KmAMilles a mitges, només el layout

---
+++ KmAMilles/app/src/main/java/edu/upc/eet/pma/kmamilles/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.upc.eet.pma.kmamilles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* onCreate és un mètode */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
